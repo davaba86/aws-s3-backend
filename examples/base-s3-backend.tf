90b6e6ae866369fb4ddc67560c6726b560ecf7ba
David Abarca <david_abarca@icloud.com>
2022-04-01 21:30:18 +0200

    Add ref to tf module declaration

--- examples/base-s3-backend.tf
+++ examples/base-s3-backend.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 module "tf_s3_backend" {
-  source = "github.com/davaba86/terraform-aws-s3-backend"
+  source = "github.com/davaba86/terraform-aws-s3-backend?ref=v0.2.0"
 
   s3_bucket_name      = "base-statefiles-${var.env_short}"
   dynamodb_table_name = "base-statelocks-${var.env_short}"

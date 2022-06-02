4cbda9d4e3e037832808cbf5824cbe4228c91bb5
David Abarca <david_abarca@icloud.com>
2022-06-02 19:51:13 +0200

    docs: :memo: Update example with TF cloud download

--- examples/base-s3-backend.tf
+++ examples/base-s3-backend.tf
@@ -24,7 +24,8 @@ resource "aws_s3_bucket_public_access_block" "server_access_loggin" {
 
 
 module "tf_s3_backend" {
-  source = "github.com/davaba86/terraform-aws-s3-backend?ref=v0.2.0"
+  source  = "davaba86/s3-backend/aws"
+  version = "0.2.5"
 
   s3_bucket_name    = "base-statefiles-${var.env_short}"
   s3_logging_bucket = "audit-server-access-logging-${var.env_short}"
